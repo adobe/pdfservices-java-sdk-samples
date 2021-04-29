ecc429e6ba8e28e2e9e82cf4691a9de8e6f8eea7
Sahil <sahkumar@adobe.com>
2021-04-29 19:50:55 +0530

    Updated Samples for the Adobe PDF Services SDK (2.0.0)

---
+++ src/main/java/com/adobe/pdfservices/operation/samples/createpdf/CreatePDFWithCustomTimeouts.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2019 Adobe
+ * All Rights Reserved.
+ *
+ * NOTICE: Adobe permits you to use, modify, and distribute this file in
+ * accordance with the terms of the Adobe license agreement accompanying
+ * it. If you have received this file from a source other than Adobe,
+ * then your use, modification, or distribution of it requires the prior
+ * written permission of Adobe.
+ */
+
+package com.adobe.pdfservices.operation.samples.createpdf;
+
+import java.io.IOException;
+
+import com.adobe.pdfservices.operation.ClientConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.adobe.pdfservices.operation.ExecutionContext;
+import com.adobe.pdfservices.operation.auth.Credentials;
+import com.adobe.pdfservices.operation.exception.SdkException;
+import com.adobe.pdfservices.operation.exception.ServiceApiException;
+import com.adobe.pdfservices.operation.exception.ServiceUsageException;
+import com.adobe.pdfservices.operation.io.FileRef;
+import com.adobe.pdfservices.operation.pdfops.CreatePDFOperation;
+
+/**
+ * This sample illustrates how to provide custom http timeouts for performing an operation. This enables the
+ * clients to set custom timeouts on the basis of their network speed.
+ * <p>
+ * Refer to README.md for instructions on how to run the samples.
+ */
+
+public class CreatePDFWithCustomTimeouts {
+
+    // Initialize the logger.
+    private static final Logger LOGGER = LoggerFactory.getLogger(CreatePDFWithCustomTimeouts.class);
+
+    public static void main(String[] args) {
+
+        try {
+
+            // Initial setup, create credentials instance.
+            Credentials credentials = Credentials.serviceAccountCredentialsBuilder()
+                    .fromFile("pdfservices-api-credentials.json")
+                    .build();
+
+            // Create client config instance with custom time-outs.
+            ClientConfig clientConfig = ClientConfig.builder()
+                    .withConnectTimeout(10000)
+                    .withSocketTimeout(40000)
+                    .build();
+
+            //Create an ExecutionContext using credentials and create a new operation instance.
+            ExecutionContext executionContext = ExecutionContext.create(credentials, clientConfig);
+            CreatePDFOperation createPDFOperation = CreatePDFOperation.createNew();
+
+            // Set operation input from a source file.
+            FileRef source = FileRef.createFromLocalFile("src/main/resources/createPDFInput.docx");
+            createPDFOperation.setInput(source);
+
+            // Execute the operation.
+            FileRef result = createPDFOperation.execute(executionContext);
+
+            // Save the result to the specified location.
+            result.saveAs("output/createPDFWithCustomTimeouts.pdf");
+
+        } catch (ServiceApiException | IOException | SdkException | ServiceUsageException ex) {
+            LOGGER.error("Exception encountered while executing operation", ex);
+        }
+    }
+}
---
+++ src/main/java/com/adobe/pdfservices/operation/samples/exportpdf/ExportPDFToJPEG.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2019 Adobe
+ * All Rights Reserved.
+ *
+ * NOTICE: Adobe permits you to use, modify, and distribute this file in
+ * accordance with the terms of the Adobe license agreement accompanying
+ * it. If you have received this file from a source other than Adobe,
+ * then your use, modification, or distribution of it requires the prior
+ * written permission of Adobe.
+ */
+
+package com.adobe.pdfservices.operation.samples.exportpdf;
+
+import java.io.IOException;
+
+import com.adobe.pdfservices.operation.ExecutionContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.adobe.pdfservices.operation.auth.Credentials;
+import com.adobe.pdfservices.operation.exception.SdkException;
+import com.adobe.pdfservices.operation.exception.ServiceApiException;
+import com.adobe.pdfservices.operation.exception.ServiceUsageException;
+import com.adobe.pdfservices.operation.io.FileRef;
+import com.adobe.pdfservices.operation.pdfops.ExportPDFOperation;
+import com.adobe.pdfservices.operation.pdfops.options.exportpdf.ExportPDFTargetFormat;
+
+/**
+ * This sample illustrates how to export a PDF file to JPEG.
+ * <p>
+ * Note that exporting a PDF file to an image format results in a ZIP archive containing one image per page.
+ * <p>
+ * Refer to README.md for instructions on how to run the samples.
+ */
+public class ExportPDFToJPEG {
+
+    // Initialize the logger.
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExportPDFToJPEG.class);
+
+    public static void main(String[] args) {
+        try {
+
+            // Initial setup, create credentials instance.
+            Credentials credentials = Credentials.serviceAccountCredentialsBuilder()
+                    .fromFile("pdfservices-api-credentials.json")
+                    .build();
+
+            //Create an ExecutionContext using credentials and create a new operation instance.
+            ExecutionContext executionContext = ExecutionContext.create(credentials);
+            ExportPDFOperation exportPdfOperation = ExportPDFOperation.createNew(ExportPDFTargetFormat.JPEG);
+
+            // Set operation input from a source file.
+            FileRef sourceFileRef = FileRef.createFromLocalFile("src/main/resources/exportPDFToImageInput.pdf");
+            exportPdfOperation.setInput(sourceFileRef);
+
+            // Execute the operation.
+            FileRef result = exportPdfOperation.execute(executionContext);
+
+            // Save the result to the specified location.
+            result.saveAs("output/exportPDFToJPEG.zip");
+
+        } catch (ServiceApiException | IOException | SdkException | ServiceUsageException ex) {
+            LOGGER.error("Exception encountered while executing operation", ex);
+        }
+    }
+}
