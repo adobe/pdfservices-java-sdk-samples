57301a09efddf2c69524b711bd94509bba55a74c
ganotra <ganotra@adobe.com>
2021-09-14 22:15:35 +0530

    Added Samples for Q3 release

---
+++ src/main/java/com/adobe/pdfservices/operation/samples/exportpdf/ExportPDFToJPEGList.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2019 Adobe
+ * All Rights Reserved.
+ *
+ * NOTICE: Adobe permits you to use, modify, and distribute this file in
+ * accordance with the terms of the Adobe license agreement accompanying
+ * it. If you have received this file from a source other than Adobe,
+ * then your use, modification, or distribution of it requires the prior
+ * written permission of Adobe.
+ */
+
+package com.adobe.pdfservices.operation.samples.exportpdf;
+
+import com.adobe.pdfservices.operation.ExecutionContext;
+import com.adobe.pdfservices.operation.auth.Credentials;
+import com.adobe.pdfservices.operation.exception.SdkException;
+import com.adobe.pdfservices.operation.exception.ServiceApiException;
+import com.adobe.pdfservices.operation.exception.ServiceUsageException;
+import com.adobe.pdfservices.operation.io.FileRef;
+import com.adobe.pdfservices.operation.pdfops.ExportPDFToImagesOperation;
+import com.adobe.pdfservices.operation.pdfops.options.exportpdftoimages.ExportPDFToImagesTargetFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * This sample illustrates how to export a PDF file to a list of JPEG files.
+ * <p>
+ * Refer to README.md for instructions on how to run the samples.
+ */
+public class ExportPDFToJPEGList {
+
+    // Initialize the logger.
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExportPDFToJPEGList.class);
+
+    public static void main(String[] args) {
+        try {
+
+            // Initial setup, create credentials instance.
+            Credentials credentials = Credentials.serviceAccountCredentialsBuilder()
+                    .fromFile("pdfservices-api-credentials.json")
+                    .build();
+
+            //Create an ExecutionContext using credentials and create a new operation instance.
+            ExecutionContext executionContext = ExecutionContext.create(credentials);
+            ExportPDFToImagesOperation exportPDFToImagesOperation = ExportPDFToImagesOperation.createNew(ExportPDFToImagesTargetFormat.JPEG);
+
+            // Set operation input from a source file.
+            FileRef sourceFileRef = FileRef.createFromLocalFile("src/main/resources/exportPDFToImageInput.pdf");
+            exportPDFToImagesOperation.setInput(sourceFileRef);
+
+            // Execute the operation.
+            List<FileRef> results = exportPDFToImagesOperation.execute(executionContext);
+
+            // Save the result to the specified location.
+            int index = 0;
+            for(FileRef result : results) {
+                result.saveAs("output/exportPDFToImagesOutput_" + index + ".jpeg");
+                index++;
+            }
+
+        } catch (ServiceApiException | IOException | SdkException | ServiceUsageException ex) {
+            LOGGER.error("Exception encountered while executing operation", ex);
+        }
+    }
+}
